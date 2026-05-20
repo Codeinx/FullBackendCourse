bedad8ec9c3594237193b4e758982666b2c0fb6d
CodeinX <aseyeged@gmail.com>
2026-05-20 08:45:40 +0100

    Add package.json and package-lock.json files; implement initial server logic with a 404 response and sample todos

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const http = require('http');
 
 const todos = [
-    
+    {id: 1, tet: 'Todo One'},
+    {id: 2, tet: 'Todo Two'},
+    {id: 3, tet: 'Todo Three'},
 ]
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('X-Powered-By', 'Node.js');
-    res.write('<h1>Hello</h1>');
-    res.write('<h2>Hello Again</h2>');
-    res.end();
+    // res.statusCode = 404;
+    // res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('X-Powered-By', 'Node.js');
+
+    res.writeHead(404, {
+       'Content-Type': 'application/json',
+       'X-Powered-By': 'Node.js' 
+    });
+
+    // res.write('<h1>Hello</h1>');
+    // res.write('<h2>Hello Again</h2>');
+    res.end(JSON.stringify({
+        success: false,
+        error: 'Not found',
+        data: null
+    }));
 });
 
 const PORT = 5000;
